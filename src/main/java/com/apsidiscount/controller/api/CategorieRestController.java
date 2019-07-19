0f378e31ab375a83be29df5e4f77e1c76ada6ada
Devv972 <henryd.pro@gmail.com>
2019-07-19 15:24:27 +0200

    ajout methode GetAllCategorie

---
+++ src/main/java/com/apsidiscount/controller/api/CategorieRestController.java
@@ -0,0 +1,23 @@
+package com.apsidiscount.controller.api;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.apsidiscount.entity.Categorie;
+import com.apsidiscount.service.CategorieService;
+
+@RestController
+public class CategorieRestController {
+	
+	@Autowired
+	private CategorieService categorieService;
+	
+	@GetMapping(produces = "application/json", path="/api/categorie")
+	public List<Categorie> getAllCategorie()  {
+		return categorieService.getAllCategorie();
+	}
+
+}
